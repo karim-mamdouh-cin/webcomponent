222a8870c1a85d6224af82021f4c7be1cb133a08
Karim Mamdouh <k.mamdouh@converted.in>
2023-12-04 13:52:14 +0200

    Initial commit

---
+++ apps/redmose-common-components/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'redmose-common-components-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'redmose-common-components';
+}
